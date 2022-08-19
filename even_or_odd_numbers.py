875ce7e42d81b9164f4cb8b88bd19d38c8286f66
Eninetjanice <71446962+Eninetjanice@users.noreply.github.com>
2022-08-19 14:57:45 +0100

    If else statement in python

---
+++ even_or_odd_numbers.py
@@ -0,0 +1,11 @@
+#program that prints even or odd numbers
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+new_number = float(number) % 2
+if new_number == 0:
+  print("This is an even number")
+else:
+  print("This is an odd number")
